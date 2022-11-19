a89c6a57b402a2f500b218737c6e800ca4589639
RICKXin9 <1970551651@qq.com>
2022-11-19 10:46:05 +0800

    update hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -1,7 +1,114 @@
 package hw2;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Percolation {
+    private int N;
+    private WeightedQuickUnionUF UF;
+    private int topNode;
+    private int bottomNode;
+    private boolean[][] open;
+    private int open_nums;
+    public Percolation(int N) {
+        if (N<=0) {
+            throw new IndexOutOfBoundsException("N need to be number > 0");
+        }
+        this.N = N;
+        UF = new WeightedQuickUnionUF(N*N+2);
+        topNode = N*N;
+        bottomNode = N*N + 1;
+        open = new boolean[N][N];
+        for (boolean[] boo1 : open) {
+            for (boolean boo2 : boo1) {
+                boo2 = false;
+            }
+        }
+        open_nums = 0;
+    }
+    public void open(int row, int col) {
+        int index = getIndex(row,col);
+        if (!isLegal(row, col)) {
+            throw new IndexOutOfBoundsException("you need to reassign row and col");
+        }
+        open_nums += 1;
+        if (row == 0 ) {
+            if (!UF.connected(index,topNode)){
+                UF.union(index,topNode);
+            }
+        }else if (row == N-1) {
+            if (!UF.connected(index,bottomNode)){
+                UF.union(index,bottomNode);
+            }
+        }
+        open[row][col] = true;
+        List<int[]> aroundList = getAround(row,col);
+        for (int[] pos:aroundList) {
+            int around_index = getIndex(pos[0], pos[1]);
+            if (!isLegal(pos[0],pos[1])) {
+                continue;
+            }
+            if (open[pos[0]][pos[1]] == true && !UF.connected(index,around_index)) {
+                UF.union(index,around_index);
+            }
+        }
+    }
+    public boolean isOpen(int row, int col) {
+        if (!isLegal(row, col)) {
+            throw new IndexOutOfBoundsException("you should give a legal coordinate");
+        }
+        return open[row][col];
+    }
+    private int getIndex(int row, int col) {
+        int index = row * N + col;
+        return index;
+    }
+    public boolean isFull(int row, int col) {
+        int index = getIndex(row, col);
+        if (UF.connected(index,topNode) && UF.connected(index, bottomNode)) {
+            return true;
+        }
+        return false;
+    }
+    public int numberOfOpenSites() {
+        return open_nums;
+    }
+    public boolean percolates() {
+        return UF.connected(topNode,bottomNode);
+    }
+    private List<int[]> getAround(int row, int col) {
+        int[] down = new int[]{row-1,col};
+        int[] up = {row+1, col};
+        int[] right = {row, col+1};
+        int[] left = {row,col-1};
+        List<int[]> around = new ArrayList<>();
+
+        around.add(down);
+        around.add(up);
+        around.add(right);
+        around.add(left);
+        return around;
+    }
+
+    private boolean isLegal(int row, int col) {
+        if (row<0 || row>N-1 || col<0 || col>N-1) {
+            return false;
+        }
+        return true;
+    }
+    public static void main(String args[]) {
+        Percolation p = new Percolation(2);
+        p.open(0,0);
+        p.open(1,0);
+        System.out.println(p.percolates());
+        System.out.println(p.isFull(0,1));
+    }
+    public int getN() {
+        return N;
+    }
+    public int getOpen_nums() {
+        return open_nums;
+    }
 
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,55 @@
 package hw2;
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
+import java.lang.IllegalArgumentException;
+import java.util.Random;
 
 public class PercolationStats {
+    private int N;
+    private int T;
+    private Percolation percolation;
+    private double[] grade;
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N<0 || T<0) {
+            throw new IllegalArgumentException("T and N need to over 0");
+        }
+        percolation = pf.make(N);
+        this.N = N;
+        this.T = T;
+    }
+    public double mean() {
 
+        return StdStats.mean(grade);
+    }
+    public double stddev() {
+        return StdStats.stddev(grade);
+    }
+    public double confidenceLow() {
+        return mean()-1.96*stddev()/Math.sqrt(T);
+    }
+    public double confidenceHigh() {
+        return mean()+1.96*stddev()/Math.sqrt(T);
+    }
+    private void getXt() {
+        double[] x_nums = new double[T];
+        for (int i=0; i<T; i++) {
+            int x_num = getOpenSites();
+
+            x_nums[i] = (double) x_num/(double) (N*N);
+        }
+        grade = x_nums;
+    }
+    private int getOpenSites() {
+
+        while (!percolation.percolates()) {
+            int row = StdRandom.uniform(percolation.getN());
+            int col = StdRandom.uniform(percolation.getN());
+            if (percolation.isOpen(row, col)) {
+                continue;
+            }
+            percolation.open(row,col);
+
+        }
+        return percolation.getOpen_nums();
+    }
 }
