9569a127c469487a0bbc8877f88cc91e5efdef66
RICKXin9 <1970551651@qq.com>
2022-12-04 17:13:30 +0800

    update lab11

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,5 +1,9 @@
 package lab11.graphs;
 
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  *  @author Josh Hug
  */
@@ -9,21 +13,51 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int s;
+    private int t;
+
+    private Maze maze;
 
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
         // Add more variables here!
+        maze = m;
+        s = maze.xyTo1D(sourceX,sourceY);
+        t = maze.xyTo1D(targetX,targetY);
+        distTo[s] = 0;
+        edgeTo[s] = s;
+        announce();
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
     private void bfs() {
         // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.add(s);
+        while (!queue.isEmpty()) {
+            int re = queue.remove();
+            marked[re] = true;
+            announce();
+            if (re == t) {
+                break;
+            }
+            for (int around:maze.adj(re)) {
+                if (!marked[around]) {
+                    edgeTo[around] = re;
+                    distTo[around] = distTo[re] + 1;
+                    announce();
+                    queue.add(around);
+                }
+
+            }
+        }
     }
 
 
     @Override
     public void solve() {
-        // bfs();
+        bfs();
     }
 }
 
--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,5 +1,10 @@
 package lab11.graphs;
+import edu.princeton.cs.algs4.LinkedStack;
+import edu.princeton.cs.algs4.Stack;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 /**
  *  @author Josh Hug
  */
@@ -9,7 +14,8 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
-
+    private boolean hasCycle = false;
+    private List<Integer> Cycle;
     public MazeCycles(Maze m) {
         super(m);
     }
@@ -17,7 +23,34 @@ public class MazeCycles extends MazeExplorer {
     @Override
     public void solve() {
         // TODO: Your code here!
+        Random random = new Random();
+        Stack<Integer> stack = new Stack<>();
+        int s = random.nextInt(maze.V());
+        stack.push(s);
+        Cycle = new ArrayList<>();
+        while (!stack.isEmpty()) {
+            int pop = stack.pop();
+            marked[pop] = true;
+            announce();
+            for (int w: maze.adj(pop)) {
+                if (!marked[w]) {
+                    stack.push(w);
+                    edgeTo[w] = pop;
+
+                    announce();
+                }else {
+                    if (edgeTo[pop] != w) {
+                        hasCycle = true;
+                        edgeTo[w] = pop;
+                        announce();
+                        Cycle.add(pop);
+                    }
+                }
+            }
+        }
+
     }
+
 
     // Helper methods go here
 }
